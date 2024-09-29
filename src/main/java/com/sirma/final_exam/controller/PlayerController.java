431a40b4d5f354c3c380c85d52e5387e8ac64820
88SvetlanaIvanova <96539287+88SvetlanaIvanova@users.noreply.github.com>
2024-09-29 12:52:43 +0300

    Update PlayerController.java
    Add a comment.

--- src/main/java/com/sirma/final_exam/controller/PlayerController.java
+++ src/main/java/com/sirma/final_exam/controller/PlayerController.java
@@ -30,4 +30,12 @@ public class PlayerController {
     public Player createPlayer(@RequestBody Player player) {
         return playerServicehandler.createPlayer(player.getTeamNumber(), player.getPosition(), player.getFullName(), player.getTeam());
     }
-}
+} /* The PlayerController class manages HTTP requests related to Player entities, providing endpoints for updating, retrieving, and creating player information.
+    The controller class follows the RESTful design pattern, handling CRUD operations for Player entities, and delegates business logic to the PlayerServiceHandler.
+    The class is annotated with @RestController to indicate it as a controller for handling RESTful requests. The @RequestMapping("/players") 
+    annotation defines the base URI for the controller. 
+    The PlayerServiceHandler dependency is injected into the controller through the constructor using the @Autowired annotation. 
+    */
+    
+    
+
